039fe1f8c1105cf4c2bb448f8d44be92edea981f
kpuk <stasionok@gmail.com>
2022-05-05 22:54:09 +0300

    +

--- app/lib/config.js
+++
@@ -1,3 +0,0 @@
-module.exports.ethPoint = 'http://192.168.0.171:8545';
-module.exports.hostPoint = 'http://127.0.0.1:3000';
-module.exports.privateKey = 'private key';
--- config/config.default.js
+++ config/config.default.js
@@ -15,7 +15,7 @@ module.exports = appInfo => {
       headerName: 'x-csrf-token',
       ignoreJSON: true,
     },
-    domainWhiteList: ['http://127.0.0.1:7001', 'http://localhost:7001'],
+    domainWhiteList: ['http://127.0.0.1:7001', 'http://localhost:7001', 'http://127.0.0.1:7001/', 'http://localhost:7001/'],
   };
   config.custom = {
     host: 'http://127.0.0.1:7001',
