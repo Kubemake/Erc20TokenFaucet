f4afb2c9b9b8540dd6d703f38796a8d222e69526
kpuk <stasionok@gmail.com>
2022-06-09 19:21:13 +0300

    upd tokens

--- app/extend/tokens.js
+++ app/extend/tokens.js
@@ -1,20 +1,8 @@
 module.exports = [
   {
-    symbol: '_TST',
-    name: '_TST',
-    address: '0x69fD2D369f89a20875CA790a5F2A305C4C1346e7',
+    symbol: 'AXSCBT',
+    name: 'AXSCBT',
+    address: '0x408B5DD6c26F1A4e4Db6D98f2fEfAa5A8F3B0611',
     limit: 6000
-  },
-  {
-    symbol: 'AMT',
-    name: 'AMT',
-    address: '0xCbAA1b9BFfF56d05185c8907A0a18374DC81273C',
-    limit: 0
-  },
-  {
-    symbol: 'AZY',
-    name: 'AZY',
-    address: '0x4Ab7C1BE7a1d4a6b4Ab00c25b6a6FFF71A8328A0',
-    limit: 0
   }
-];
+]
