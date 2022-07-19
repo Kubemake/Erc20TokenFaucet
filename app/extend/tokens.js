c940f8a78ae43d1bd34c98c7336075246b1f57d0
kpuk <stasionok@gmail.com>
2022-07-19 17:24:43 +0300

    limit AMT/AZY/_TST for amazy

--- app/extend/tokens.js
+++ app/extend/tokens.js
@@ -3,18 +3,18 @@ module.exports = [
     symbol: '_TST',
     name: '_TST',
     address: '0x69fD2D369f89a20875CA790a5F2A305C4C1346e7',
-    limit: 6000
+    limit: 1000
   },
   {
     symbol: 'AMT',
     name: 'AMT',
     address: '0xCbAA1b9BFfF56d05185c8907A0a18374DC81273C',
-    limit: 0
+    limit: 1000
   },
   {
     symbol: 'AZY',
     name: 'AZY',
     address: '0xABaa65c909619C5ebbDF993f9AF5f436A8a83736',
-    limit: 0
+    limit: 1000
   }
 ];
