a044ebabbb401123d270b4c9e7de220ffa5ab0d9
kpuk <stasionok@gmail.com>
2022-08-30 14:45:28 +0300

    +

--- app/extend/tokens.js
+++ app/extend/tokens.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
-    symbol: '_TST',
-    name: '_TST',
+    symbol: '_TST2',
+    name: '_TST2',
     address: '0xd006D930993b32aC774113b5fF57b1c71167c0EA',
     limit: 6000
   }
