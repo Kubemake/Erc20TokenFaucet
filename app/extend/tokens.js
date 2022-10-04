50c418b5d0e8c3cf83e9028994c5e0ca9fbad33a
kpuk <stasionok@gmail.com>
2022-10-04 13:38:38 +0300

    return tst2

--- app/extend/tokens.js
+++ app/extend/tokens.js
@@ -4,5 +4,11 @@ module.exports = [
     name: '_TST3',
     address: '0xb0D2e96602cBcDc1d0f91b3ABf6EF7521D1A2C06',
     limit: 6000
+  },
+  {
+    symbol: '_TST2',
+    name: '_TST2',
+    address: '0xd006D930993b32aC774113b5fF57b1c71167c0EA',
+    limit: 6000
   }
 ]
