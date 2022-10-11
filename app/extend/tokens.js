76317147826b1fba539c3e331a424f5aae1dcdcc
kpuk <stasionok@gmail.com>
2022-10-11 20:39:51 +0300

    add tst4 token for axes

--- app/extend/tokens.js
+++ app/extend/tokens.js
@@ -1,5 +1,5 @@
 module.exports = [
-  {
+  /*{
     symbol: '_TST3',
     name: '_TST3',
     address: '0xb0D2e96602cBcDc1d0f91b3ABf6EF7521D1A2C06',
@@ -9,6 +9,12 @@ module.exports = [
     symbol: '_TST2',
     name: '_TST2',
     address: '0xd006D930993b32aC774113b5fF57b1c71167c0EA',
+    limit: 6000
+  },*/
+  {
+    symbol: 'TST4',
+    name: 'TST4',
+    address: '0x0886dC84B4263d3A9420B90CB2b185407a4D41e3',
     limit: 6000
   }
 ]
